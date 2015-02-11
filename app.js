9594d5cd5e961d52a091419679e17c0d2f143400
Alex Ung <alexung.ucsb@gmail.com>
2015-02-10 17:44:05 -0800

    separating the different templates into scripts. html in javascript, what??

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
-var myApp = angular.module('hackerNews', []);
+var myApp = angular.module('hackerNews', ['ui.router']);
+// we included ui.router in the script we included in index.html
+myApp.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: '/home.html',
+    controller: 'MainCtrl'
+  });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+myApp.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+// by angular conventions, lowerCamelCase is used for factory names that won't be new'd
+// factory and service are similar, but nuanced.  Can check out the difference at http://tylermcginnis.com/angularjs-factory-vs-service-vs-provider/
+
+// myApp.state('posts', {
+//   url: '/posts/{id}',
+//   templateUrl: '/posts.html',
+//   controller: 'PostsCtrl'
+// });
+
+myApp.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
+
+}]);
 
 myApp.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
 
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') {return;}
-    $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+    $scope.posts.push({title: $scope.title,
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Alex', body: 'This post is teh most awesome Ive ever seen', upvotes: 0}
+        ]
+      });
     $scope.title = ''; // to clear the form after submit has been entered
     $scope.link = '';
   };
